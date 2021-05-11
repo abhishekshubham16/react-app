d737414a13960c64deddeab0b12a79c7d1a52f11
abhishekshubham16 <abhishekshubham087@gmail.com>
2021-05-11 18:27:43 +0530

    event handling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ let App = () => {
   let newTime = new Date().toLocaleTimeString();
 
   const [ctime, setTime] = useState(newTime);
+  const red = '#FF0000';
+  const[bg, setbg]=useState(red);
+  const[name, setname] = useState('click me');
 
   const updateTime = () =>{
     newTime = new Date().toLocaleTimeString();
@@ -13,12 +16,26 @@ let App = () => {
   }
 
   setInterval(updateTime,1000)
+
+  const bgchange = () =>{
+    let newbg = '#34495e';
+    setbg(newbg);
+    setname('Ouch!! :')
+  }
+
+  const bgback = () => {
+    setbg(red);
+    setname('Change!:')
+  }
   return(
     <>
       <Parent/>
       <Child/>
       <h1>{ctime}</h1>
       {/* <button onClick={updateTime}>get time</button> */}
+      <div style={{backgroundColor : bg}}>
+        <button onClick={bgchange} onDoubleClick={bgback}> {name} </button>
+      </div>
     </>
   )
 }
