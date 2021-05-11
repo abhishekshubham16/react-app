8ed190b2c9d9869c8f2e504798cbed888b6f090d
abhishekshubham16 <abhishekshubham087@gmail.com>
2021-05-11 18:10:13 +0530

    add useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import Parent from './components/Parent';
+import React, {useState} from 'react';
 import Child from './components/Child';
+import Parent from './components/Parent';
+
 let App = () => {
+  let newTime = new Date().toLocaleTimeString();
+
+  const [ctime, setTime] = useState(newTime);
+
+  const updateTime = () =>{
+    newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  setInterval(updateTime,1000)
   return(
     <>
-      <h1>App Component</h1>
       <Parent/>
       <Child/>
+      <h1>{ctime}</h1>
+      {/* <button onClick={updateTime}>get time</button> */}
     </>
   )
 }
